576a7e78e56f970465f2bfe58cc8ce94c07248b1
Tyler-Browning <tbrowning@stu.naperville203.org>
2023-10-27 14:01:56 -0500

    skeletons

---
+++ src/maze.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class maze 
+{
+    private square squares[][];
+
+    public boolean loadMaze(String fName)
+    {
+
+    }
+
+    public ArrayList<square> getNeighbors(square sq)
+    {
+
+    }
+
+    public square getStart() {
+
+    }
+    public square getFinish() {
+
+    }
+
+    public void reset() {
+
+    }
+
+    public String toString() {
+        
+    }
+}
